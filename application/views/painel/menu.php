335c575dd011f4988fb912e9d01ccb08cd38fe7e
benedito <benethowen@yahoo.com.br>
2022-01-17 19:45:02 -0300

    Concluido os métodos de salvar e editar videos.

--- application/views/painel/menu.php
+++ application/views/painel/menu.php
@@ -3,6 +3,7 @@
           <nav class="nav nav-masthead justify-content-center">
             <a class="nav-link active" target='_blank' href="<?php echo base_url();?>">ver site</a>
             <a class="nav-link" href="<?php echo base_url('index.php/post'); ?>">posts</a>
+            <a class="nav-link" href="<?php echo base_url('index.php/video'); ?>">videos</a>
             <a class="nav-link" href="<?php echo base_url('index.php/setup'); ?>">configs</a>
             <a class="nav-link" href="<?php echo base_url('index.php/setup/logout'); ?>">sair</a>
         
---
+++ application/views/painel/videos.php
@@ -0,0 +1,107 @@
+<?php $this->load->view('painel/header'); ?>
+
+<div class="coluna col2">
+    <ul class="sem-marcador sem-padding">
+        <li><a href="<?php echo base_url('index.php/video/cadastrar'); ?>">INSERIR</a></li>
+        <li><a href="<?php echo base_url('index.php/video/listar')  ?>">LISTAR</a></li>
+
+</div>
+    <div class="coluna col10">
+        <h2><?php echo $h2; ?></h2>
+
+        <?php 
+            if($msg = get_msg()):
+                echo '<div class="msg-box">'.$msg.'</div>';
+            endif;
+
+            switch ($tela):
+                case 'listar':
+                    if(isset($videos) && sizeof($videos) > 0):
+                        ?>
+                        <table>
+                            <thead>
+                                <th align="left">Título</th>
+                                <th align="right">Ações</th>
+                            </thead>
+                            <tbody>
+                                <?php
+                                foreach ($videos as $linha):
+                                    ?>
+                                    <tr>
+                                        <td class="titulo-post"><?php echo $linha->titulo; ?></td>
+                                        <td align="right" class="acoes"><?php echo anchor('video/editar/'.$linha->id, 'Editar'); ?> | 
+                                        <?php echo anchor('video/excluir/'.$linha->id, 'Excluir');?> |
+                                        <?php echo anchor('videoaulas/'.$linha->id, 'Ver', array('target'=>'_blank')); ?> </td>
+
+                                    </tr>
+                                    <?php
+                                endforeach;    
+
+                                ?>
+                            </tbody>
+                        </table>
+                        <?php
+                    else:
+                       echo '<div class="msg-box"><p>Nenhum post cadastrado!</p></div>';
+                    endif; 
+                break;
+                case 'cadastrar':
+                    echo form_open_multipart();
+                    echo form_label('Título:', 'titulo');
+                    echo form_input('titulo', set_value('titulo'));
+                    echo form_label('descrição:', 'descricao');
+                    echo form_textarea('descricao', to_html(set_value('descricao')), array('class'=>'editorhtml'));
+                    echo form_label('link', 'link');
+                    echo form_input('link', set_value('link'));
+                    echo form_label('data', 'data');
+                    echo form_input('data', set_value('data'));
+                    echo form_label('Imagem do post (thumbnail):', 'imagem');
+                    echo form_upload('imagem');
+                    echo form_submit('enviar', 'Salvar post', array('class'=>'botao'));
+                    echo form_close();
+
+                break;
+                case 'editar':
+                    echo form_open_multipart();
+                    echo form_label('Título:', 'titulo');
+                    echo form_input('titulo', set_value('titulo', to_html($videos->titulo)));
+                    echo form_label('Descrição:', 'descricao');
+                    echo form_textarea('descricao', to_html(set_value('descricao', to_html($videos->descricao))), array('class'=>'editorhtml'));
+                    echo form_label('link', 'link');
+                    echo form_input('link', set_value('link'));
+                    echo form_label('data', 'data');
+                    echo form_input('data', set_value('data'));
+                    echo form_label('Imagem do post (thumbnail):', 'imagem');
+                    echo form_upload('imagem');
+                    // miniatura da imagem recuperada do BD
+                    echo '<p><small>Imagem atual:</small><br /><img src=" '.base_url('uploads/'.$videos->imagem).'" class="thumb-edicao"/></p>';
+                    
+             
+                    echo form_submit('enviar', 'Salvar vídeo', array('class'=>'botao'));
+                    echo form_close();
+                break;
+                case 'excluir':
+                    echo form_open_multipart();
+                    echo form_label('Título:', 'titulo');
+                    echo form_input('titulo', set_value('titulo', to_html($posts->titulo)));
+                    echo form_label('Conteúdo:', 'conteudo');
+                    echo form_textarea('conteudo', to_html(set_value('conteudo', to_html($posts->conteudo))), array('class'=>'editorhtml'));
+                    // miniatura da imagem recuperada do BD
+                    echo '<p><small>Imagem:</small><br /><img src=" '.base_url('uploads/'.$posts->imagem).'" class="thumb-edicao"/></p>';
+                    
+             
+                    echo form_submit('enviar', 'Excluir post', array('class'=>'botao'));
+                    echo form_close();
+                break;
+            endswitch;
+
+        
+
+
+
+        ?>
+        
+    </div>
+<div class="coluna col3">&nbsp;</div>
+
+<?php $this->load->view('painel/footer'); ?>
