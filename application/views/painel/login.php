618b37c825deb7e7eb08e4f7eb5b9110e9b8d6f4
benedito <benethowen@yahoo.com.br>
2022-02-04 22:12:17 -0300

    Acrescentado css bootstrap no login

--- application/views/painel/login.php
+++ application/views/painel/login.php
@@ -16,19 +16,55 @@
             <?php 
                 if($msg = get_msg()):
                     echo '<div class="msg-box">'.$msg.'</div>';
-                endif;
+                    endif;
+                   echo form_open();
+                    ?>
 
-                echo form_open();
-                echo form_label('Usuário', 'login');
-                echo form_input('login', set_value('login'), array('autofocus' => 'autofocus'));
-                echo form_label('Senha: ', 'senha');
-                echo form_password('senha');
-                echo form_submit('enviar',  'Autenticar', array('class'=> 'botao'));
-                echo form_close();
+            <form  class="dropdown-menu p-4">
+            <div class="mb-3">
+                <label id="usuario" name="usuario" for="usuario" class="form-label">Login</label>
+                <input type="text" class="form-control" for="login" id="login" name="login" value="login" placeholder="email@example.com">
+                <label id="senha" name="senha" for="senha" class="form-label">Senha</label>
+                <input type="password" class="form-control" id="senha" name="senha" value="senha" placeholder="senha">
+               
+                <button type="submit" class="btn btn-primary">Autenticar</button>
+         
+        </div>
+    </form>
+    
+    <?php 
+             echo form_close();
+             ?>
+            
+            
+            
+               <!-- Button trigger modal -->
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+               Launch demo modal
+             </button>
 
 
-'                       '
-            ?>
+
+
+
+             <!-- Modal -->
+ <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+   <div class="modal-dialog">
+     <div class="modal-content">
+       <div class="modal-header">
+         <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+       </div>
+       <div class="modal-body">
+         ...
+       </div>
+       <div class="modal-footer">
+         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+         <button type="button" class="btn btn-primary">Save changes</button>
+       </div>
+     </div>
+   </div>
+ </div>
             
         </div>
     
